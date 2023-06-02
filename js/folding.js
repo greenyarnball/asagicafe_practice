ddab8772e3148386964c03656fce335f496e7d31
greenyarnball <77083012+greenyarnball@users.noreply.github.com>
2023-06-02 15:31:49 +0900

    試しに全削除

--- js/folding.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('.title').on('click',function() {
-		$(this).toggleClass('selected');
-		$(this).next().slideToggle();
-		$('.title').not($(this)).next().slideUp();
-		$('.title').not($(this)).removeClass('selected');
-	});
-});
